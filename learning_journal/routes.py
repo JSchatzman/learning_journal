9424eb9230fc4de820689b6d5236207ac7a4e621
jordan <j.schatzman@outlook.com>
2016-12-23 14:11:25 -0800

    copying over code and adding default entries to db

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -1,3 +1,10 @@
+"""Include these routes in deployment."""
+
+
 def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    """Pyramid route configuration."""
+    config.add_static_view('static', 'static', cache_max_age=10)
     config.add_route('home', '/')
+    config.add_route('detail', '/journal/{id:\d+}')
+    config.add_route('create', '/journal/new-entry')
+    config.add_route('update', '/journal/{id:\d+}/edit-entry')
